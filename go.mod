bab713fc586ee34a776967941499ba3f8a34c065
Clayton Townsend II <clayton@claytontii.com>
2022-10-29 13:42:37 -0500

    migrate errors to sentinel errors, and wrap errors from external packages

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/simplylib/ucheck
 
 go 1.18
 
-require golang.org/x/mod v0.6.0
+require (
+	github.com/simplylib/multierror v0.0.1
+	golang.org/x/mod v0.6.0
+)
--- godep/modproxy/modproxy.go
+++ godep/modproxy/modproxy.go
@@ -3,11 +3,14 @@ package modproxy
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/simplylib/multierror"
 )
 
 type Module struct {
@@ -23,44 +26,34 @@ type ModProxy struct {
 func (p ModProxy) ListVersions(ctx context.Context, modulePath string) ([]string, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", p.Endpoint+"/"+modulePath+"/@v/list", nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not create new Request (%w)", err)
 	}
 
-	var res *http.Response
-	res, err = http.DefaultClient.Do(req)
+	var resp *http.Response
+	resp, err = http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not Do HTTP Request (%W)", err)
 	}
 
 	defer func() {
-		if err == nil {
-			return
+		if err2 := resp.Body.Close(); err != nil {
+			err = multierror.Append(err, fmt.Errorf("could not close Response Body (%w)", err2))
 		}
-		err = res.Body.Close()
 	}()
 
 	var resBytes []byte
-	resBytes, err = io.ReadAll(res.Body)
+	resBytes, err = io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not ReadAll bytes from Response Body (%w)", err)
 	}
 
 	if len(resBytes) <= 1 {
-		err = res.Body.Close()
-		if err != nil {
-			return nil, err
-		}
 		return nil, nil
 	}
 
 	vs := strings.Split(string(resBytes[:len(resBytes)-1]), "\n")
 	versions := make([]string, len(vs))
 	copy(versions, vs)
-
-	err = res.Body.Close()
-	if err != nil {
-		return nil, err
-	}
 
 	return versions, nil
 }
@@ -72,31 +65,47 @@ type Info struct {
 	Time time.Time `json:"time"`
 }
 
-//GetLatestVersion of module defined by modulePath
+var ErrModuleNotFound = errors.New("modproxy: module not found")
+
+type ErrProxyNotOK struct {
+	StatusCode int
+	Err        string
+}
+
+func (e ErrProxyNotOK) Error() string {
+	return e.Err
+}
+
+// GetLatestVersion of module defined by modulePath
 func (p ModProxy) GetLatestVersion(ctx context.Context, modulePath string) (Info, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", p.Endpoint+"/"+modulePath+"/@latest", nil)
 	if err != nil {
-		return Info{}, err
+		return Info{}, fmt.Errorf("could not create new Request (%w)", err)
 	}
 
 	var resp *http.Response
 	resp, err = http.DefaultClient.Do(req)
 	if err != nil {
-		return Info{}, err
+		return Info{}, fmt.Errorf("could not Do HTTP Request (%w)", err)
 	}
 
 	defer func() {
-		if err == nil {
-			return
+		if err2 := resp.Body.Close(); err2 != nil {
+			err = multierror.Append(err, fmt.Errorf("could not close Response Body (%w)", err2))
 		}
-		err = resp.Body.Close()
 	}()
 
 	if resp.StatusCode != http.StatusOK {
 		if resp.StatusCode == http.StatusNotFound {
-			return Info{}, fmt.Errorf("modproxy: module not found")
+			return Info{}, ErrModuleNotFound
 		}
-		return Info{}, fmt.Errorf("modproxy: http server error")
+
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return Info{}, fmt.Errorf("could not ReadAll from Response Body (%w)", err)
+		}
+
+		return Info{}, ErrProxyNotOK{StatusCode: resp.StatusCode, Err: string(b)}
 	}
 
 	decoder := json.NewDecoder(resp.Body)
@@ -104,12 +113,12 @@ func (p ModProxy) GetLatestVersion(ctx context.Context, modulePath string) (Info
 	var info Info
 	err = decoder.Decode(&info)
 	if err != nil {
-		return Info{}, err
+		return Info{}, fmt.Errorf("could not Json decode Response from /@latest goproxy endpoint (%w)", err)
 	}
 
 	err = resp.Body.Close()
 	if err != nil {
-		return Info{}, err
+		return Info{}, fmt.Errorf("could not close Response Body (%w)", err)
 	}
 
 	return info, nil
