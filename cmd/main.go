cb74abea72571680cd35df32c899f60c67859cfd
Clayton Townsend II <clayton@claytontii.com>
2022-11-15 16:47:12 -0600

    add a simple interface to godep as default command in cmd/main

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,67 @@
 package cmd
 
-import "log"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/simplylib/ucheck/godep"
+	"github.com/simplylib/ucheck/godep/modproxy"
+)
 
 func run() error {
+	goProxy := flag.String("goproxy", "https://proxy.golang.org", "base url of go proxy server")
+
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintln(
+			flag.CommandLine.Output(),
+			"Usage: "+os.Args[0]+" <flags> <project dir> <project dir 2>",
+		)
+		fmt.Fprintln(
+			flag.CommandLine.Output(),
+			"Project directory is optional and can be multiple directories seperated by a space, defaults to current directory",
+		)
+		flag.CommandLine.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	var paths []string
+	if flag.NArg() == 0 {
+		wd, err := os.Getwd()
+		if err != nil {
+			return fmt.Errorf("could not get the current directory (%w)", err)
+		}
+
+		paths = []string{wd}
+	} else {
+		paths = flag.Args()
+	}
+
+	var (
+		err     error
+		updates []godep.Update
+		buf     []byte
+	)
+	for i := range paths {
+		buf, err = os.ReadFile(filepath.Join(paths[i], string(filepath.Separator)+"go.mod"))
+		if err != nil {
+			return fmt.Errorf("could not read file (%v) error (%w)", paths[i], err)
+		}
+
+		updates, err = godep.CheckGoModBytesForUpdates(context.Background(), modproxy.ModProxy{Endpoint: *goProxy}, buf)
+		if err != nil {
+			return fmt.Errorf("could not check (%v) for updates due to error (%w)", filepath.Join(paths[i], string(filepath.Separator)+"go.mod"), err)
+		}
+
+		if len(updates) != 0 {
+			log.Printf("path (%v) has updates\n", paths[i])
+		}
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.18
 
 require (
 	github.com/simplylib/multierror v0.0.1
-	golang.org/x/mod v0.6.0
+	golang.org/x/mod v0.7.0
 )
